9521c3a4baccd91b8ab00a5baf0fce605873dc50
brunolaguna <bruno_cs1.8@hotmail.com>
2023-09-24 17:28:15 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TypeScript MERN Recylced-E-commerce
+
+Aqui já foi intalado as ferramentas necessária por enquanto para a criação do projeto, criamos o TSX App pelo Vite e foi criado o repositório do GitHub.
---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <header>
+        Recycled E-commerce
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        Todos os direitos são reservados
+      </footer>
+    </div>
+  )
+}
+
+export default App
